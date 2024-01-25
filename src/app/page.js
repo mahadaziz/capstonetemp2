9d88f7cc5fc69a9ff5001560f01ebc28fd61bfe1
mahadaziz <59572398+mahadaziz@users.noreply.github.com>
2024-01-24 19:52:21 -0500

    Added counter

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
+'use client'
 import Image from "next/image";
+import React, { useState } from 'react';
 
 export default function Home() {
+  // Initialize the counter state
+  const [counter, setCounter] = useState(0);
+
+  // Function to increment the counter
+  const incrementCounter = () => {
+      setCounter(counter + 1);
+  };
+
   return (
     <div>
-      Hello World
+      <div>
+        Hello World
+      </div>
+      <div>
+        <p>Counter: {counter}</p>
+        <button onClick={incrementCounter}>Increment Counter</button>
+      </div>
     </div>
   );
 }
